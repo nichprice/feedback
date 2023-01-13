fb569cc46f5251eaa0b154c39e9d970b1f12031d
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-01-12 20:06:54 -0500

    operating form

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+    
+    else:
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html", {
+        "form": form
+    })
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
